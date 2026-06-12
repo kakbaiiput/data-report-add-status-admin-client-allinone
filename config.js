454b45a6ea3f68abcd6cb3b1a1d3b1b52556b723
kakbaiiput <kakbaiiput@gmail.com>
2026-06-12 15:37:58 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,29 @@
+const CONFIG = {
+
+    API_URL: 'https://script.google.com/macros/s/AKfycbwE9TamMNcJgqTBCXgcZqS40jmxK_nA4sFWiKu3cZpdgwcluVWQovxnXD3LnNhz6UZ9/exec',
+
+    VPS_API_URL: 'https://report.octolink.id/api/data.php',
+
+    API_TIMEOUT: 35000,
+
+    RETRY_ATTEMPTS: 2,
+
+    VALIDATE_ORIGIN: false,
+
+    FORCE_MOBILE_LAYOUT: false,
+
+    DEBUG_MODE: false,
+
+
+
+    getApiUrl(type) { return this.API_URL; },
+
+    getCurrentOrigin() { return window.location.origin; },
+
+    log(...args) { if (this.DEBUG_MODE) console.log('[STARLINK]', ...args); },
+
+    warn(...args) { console.warn('[STARLINK WARN]', ...args); },
+
+    error(...args) { console.error('[STARLINK ERROR]', ...args); }
+
+};
